00d4d3dfc996c96bf3aef71f5546548f3b77800d
Sjofil <Sjowall.filip@gmail.com>
2021-11-07 16:30:20 +0100

    Mall för html nu klar!

--- Website/testing.py
+++ Website/testing.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<name>")
 def index(name=None):
-    return render_template("start.html", name=name)
+    return render_template("index.html", name=name)
 
 if __name__=="__main__":
     app.run()
