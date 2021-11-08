051cfc4409ed7c5505467e592e867371349d6fe8
Pontus Persson <8782054+DrGurka@users.noreply.github.com>
2021-11-08 15:05:55 +0100

    La till routes till alla sidor

--- Website/__init__.py
+++ Website/__init__.py
@@ -32,7 +32,10 @@ def create_app(test_config=None):
     #from . import db
     #db.init_app(app)
 
-    from . import auth
-    app.register_blueprint(auth.bp)
+    from . import common
+    app.register_blueprint(common.bp)
+
+    from . import questions
+    app.register_blueprint(questions.bp)
 
     return app
