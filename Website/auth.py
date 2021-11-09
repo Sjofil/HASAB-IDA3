1387f0d21afa778f0e32bb37690d706a19ca64be
Anna Bergvall <anna.v.bergvall@gmail.com>
2021-11-09 10:54:47 +0100

    Update auth.py

--- Website/auth.py
+++ Website/auth.py
@@ -7,3 +7,20 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 bp = Blueprint('auth', __name__, url_prefix='/')
 
+@bp.route("/")
+def login():
+    if request.method == 'POST':
+        username = request.form['Användarnamn']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM users WHERE username = ?', (username,)
+        ).fetchone()
+        
+        if user is None:
+            error = 'Ogiltigt användarnamn.'
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['']
+        )
