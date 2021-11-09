3e4afd7e4bdb298d6ddc34f328df9a89c6d48639
Pontus Persson <8782054+DrGurka@users.noreply.github.com>
2021-11-09 11:03:24 +0100

    Fixade syntaxfel i auth

--- Website/auth.py
+++ Website/auth.py
@@ -4,10 +4,11 @@ from flask import(
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
+from . import db
 
 bp = Blueprint('auth', __name__, url_prefix='/')
 
-@bp.route("/")
+@bp.route("/auth")
 def login():
     if request.method == 'POST':
         username = request.form['Användarnamn']
@@ -23,4 +24,4 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['']
-        )
+    return render_template("Admin-html/admin-log-in.html")
