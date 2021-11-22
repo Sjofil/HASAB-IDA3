2cd31fc8cf618f6585261c706aab1254159a210e
Sjofil <Sjowall.filip@gmail.com>
2021-11-22 14:02:41 +0100

    Design ändringar

--- Website/static/js/script-question.js
+++ Website/static/js/script-question.js
@@ -1,6 +1,6 @@
 
 const submitButton = document.getElementById("next");
-const input = document.getElementById("answer");
+const forwardButton = document.getElementById("forward");
 const radioButton = document.getElementById("radio1");
 const radioButton2 = document.getElementById("radio2");
 const totalPages = document.getElementById("totalPages").childElementCount;
@@ -19,52 +19,31 @@ window.addEventListener('load', function() {
   
   })
 
-//disablar knappen tills man skrivit in något
-console.log(input);
-
-input.addEventListener('keyup', (e) => {
-    console.log(e.currentTarget.value);
-
-    const value = e.currentTarget.value;
-    if(value === ""){
-        submitButton.disabled = true;
-    } else {
-        submitButton.disabled=false;
-    }
-});
-
-//Enables input "answer" and clears input "answer" after radiobutton change
-document.body.addEventListener('change', (e) => {
-    
-   value = e.target;
-    
-    // if radio1 is pressed
-    if(value.id === 'radio1'){
-        input.disabled = false;
-        if(change){
-           submitButton.disabled = false;
-        }else{
-            submitButton.disabled = true;
-        }
-        change = true;
-    }
-    // if radio2 is pressed, disables 
-    if(value.id === 'radio2'){ 
-        input.disabled = true;
-        submitButton.disabled = false;
-        change = false;
-        document.getElementById("answer").value = 5;
-    } 
-   
-});
 
 function submitForm(){
 document.location.href="last.html";
 }
 
+function forward(){
+    var page = getRequestVariable("page");
+    var pageNumber = parseInt(page);
+    console.log("Page: " + page + " Total: " + totalPages);
+    if(page != undefined && page != NaN) 
+    {
+        if(totalPages > pageNumber){
+          
+            document.location.href="/question?page=" + (pageNumber + 1);
+        }
+        else if(totalPages == pageNumber) {
+            document.location.href="/send-form";
+        }
+    }
+}
+
 function nextQuest(){
     var page = getRequestVariable("page");
     var pageNumber = parseInt(page);
+    console.log("Page: " + page + " Total: " + totalPages);
     if(page != undefined && page != NaN) 
     {
         if(totalPages > pageNumber){
@@ -101,6 +80,36 @@ function sendCookie(){
     });
 }
 
+function back(){
+    var page = getRequestVariable("page");
+    var pageNumber = parseInt(page);
+    if(page != undefined && page != NaN) 
+    {
+        if(pageNumber > 1){
+            document.location.href="/question?page=" + (pageNumber - 1);
+        }
+        
+    }
+    else
+    {
+        document.location.href="/question?page=2"
+    }
+
+}
+
+
+function sendCookie(){
+    let answers = getCookie("answers");
+    console.log(answers)
+    $.ajax({
+        url: "/send-form",
+        type: 'POST',
+        data :{
+            answers: answers
+        },
+    });
+}
+
 function prevQuest(){
     var page = getRequestVariable("page");
     var pageNumber = parseInt(page);
@@ -117,6 +126,8 @@ function prevQuest(){
     }
 
 }
+
+
 
 
     function getSelectedValue(){
