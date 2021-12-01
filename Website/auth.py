d5b7c4e5d6ed7b2600e4aedfa094053277c0a9b4
Anna Bergvall <anna.v.bergvall@gmail.com>
2021-12-01 13:42:27 +0100

    Update auth.py
    fixade bug - funkar att printa svar från företag som FINNS i db

--- Website/auth.py
+++ Website/auth.py
@@ -10,10 +10,6 @@ from . import db
 
 bp = Blueprint('auth', __name__, url_prefix='/')
 
-@bp.route("/reportTemplate")
-def reportTemplate():
-    return render_template("Admin-html/reportTemplate.html")
-
 @bp.route("/admin", methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
@@ -52,8 +48,6 @@ def adminLoggedIn():
             print("clearing session")
             session.clear()
             return redirect(url_for('auth.login'))
-
-        #Add user
         if(request.form['submit']=="addUser"):
             print(request.form)
             stmt="INSERT INTO `user` (`Type_ID`, `Adress`, `Name`) VALUES (%s, %s, %s)"
@@ -62,17 +56,6 @@ def adminLoggedIn():
             cursor=conn.cursor()
             cursor.execute(stmt, (request.form['branch'], request.form['adress'], request.form['name']))
             conn.commit()
-            
-            
-        if(request.form['submit']=="Resturang"): #Value="Resturang", "Hotell", "Byrå", "Anläggning"          
-            return redirect(url_for('auth.reportTemplate'))
-        if(request.form['submit']=="Hotell"):
-            return redirect(url_for('auth.reportTemplate'))
-        if(request.form['submit']=="Byrå"):
-            return redirect(url_for('auth.reportTemplate'))
-        if(request.form['submit']=="Anläggning"):
-            return redirect(url_for('auth.reportTemplate'))
-    
         if(request.form['submit']=="changePassword"):
             if(request.form['pass1']==request.form['pass2']):
                 stmt="UPDATE `admin` SET `password` = %s WHERE (`name` = %s)"
@@ -87,22 +70,19 @@ def adminLoggedIn():
         # Om admin sökt en enskild rapport 
         if(request.form['submit']=="findReport"):
             name = request.form['name']
-            if (ifPresent("Name", name)):
-                print(request.form)
-                stmt = "SELECT distinct Question_text, Value, Name from answers, questions, user \
-                     where questions.ID = Question_ID and Name = (%s)"
-                conn=db.get_db()
-                cursor=conn.cursor()
-                print(stmt)
-                print("hit funkar det") # yes hit funkar det
-                cursor.execute(stmt, (request.form['name']))
-                records = cursor.fetchall()
-                print(records)
-                for row in records:
-                    print(row[0], " = ", row[1])
-            else:
-                error = "Användaren finns inte"
-                flash(error)
+            #if (ifPresent("Name", name)):
+            print(request.form)
+            stmt = "SELECT distinct Question_text, Value, Name from answers, questions, user \
+             where questions.ID = Question_ID and Name = (%s)"
+            conn=db.get_db()
+            cursor=conn.cursor()
+            print(stmt)
+            print("hit funkar det") # yes hit funkar det
+            cursor.execute(stmt, (request.form['name']))
+            records = cursor.fetchall()
+            print(records)
+            for row in records:
+                print(row[0], " = ", row[1])
         
     return render_template("Admin-html/admin-index.html")
     
