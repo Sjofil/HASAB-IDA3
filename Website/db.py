601576b4e79ccafec494a6f93b674d6606850994
Pontus Persson <8782054+DrGurka@users.noreply.github.com>
2021-12-03 13:04:10 +0100

    La till en init fil för databasen

--- Website/db.py
+++ Website/db.py
@@ -25,6 +25,10 @@ def close_db(e=None):
 def init_db():
     db = get_db()
 
+    with current_app.open_resource('schema.sql') as f:
+        db.cursor().execute(f.read().decode('utf8'))
+        db.commit()
+
 
 @click.command('init-db')
 @with_appcontext
