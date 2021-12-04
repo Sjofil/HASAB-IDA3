c66f2d121204d8a5b184547e16b62675f4d1e65b
Anna Bergvall <anna.v.bergvall@gmail.com>
2021-12-04 14:26:12 +0100

    Update create_schema copy.sql

--- Databas/create_schema copy.sql
+++ Databas/create_schema copy.sql
@@ -1,21 +1,26 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE [IF NOT EXISTS] Types (
+DROP TABLE Types;
+DROP TABLE Users;
+DROP TABLE Questions;
+DROP TABLE Answers;
+
+CREATE TABLE Types (
   ID int NOT NULL,
-  PRIMARY_KEY (ID)
+  PRIMARY KEY (ID)
 );
 
-CREATE TABLE [IF NOT EXISTS] Users (
+CREATE TABLE Users (
   Email varchar(45),
   Name varchar (45),
   Type_ID int DEFAULT NULL,
   Submitted tinyint DEFAULT NULL,
   PRIMARY KEY (Email),
   KEY type_idx (Type_ID),
-  FOREIGN_KEY (Type_ID) REFERENCES Types (ID)
+  FOREIGN KEY (Type_ID) REFERENCES Types (ID)
 );
 
-CREATE TABLE [IF NOT EXISTS] Questions (
+CREATE TABLE Questions (
   ID int NOT NULL AUTO_INCREMENT,
   Type_ID int DEFAULT NULL,
   Next_question longtext,
@@ -25,16 +30,16 @@ CREATE TABLE [IF NOT EXISTS] Questions (
 );
 
 
-CREATE TABLE [IF NOT EXISTS] Answers (
+CREATE TABLE Answers (
   ID int NOT NULL AUTO_INCREMENT,
-  Value varchar(45) NOT NULL,
+  Answer varchar(45) NOT NULL,
   Question_ID int DEFAULT NULL,
   User_ID int DEFAULT NULL,
   UNIQUE KEY index_UNIQUE (ID),
-  KEY `qtype_idx` (`Question_ID`),
-  KEY `utype_idx` (`User_id`),
-  CONSTRAINT `qtype` FOREIGN KEY (`Question_ID`) REFERENCES `questions` (`ID`),
-  CONSTRAINT `utype` FOREIGN KEY (`User_id`) REFERENCES `user` (`ID`)
+  KEY qtype_idx (Question_ID),
+  KEY utype_idx (User_id),
+  CONSTRAINT qtype FOREIGN KEY (Question_ID) REFERENCES questions (ID), 
+  CONSTRAINT utype FOREIGN KEY (User_id) REFERENCES user (ID)
 );
 
 
