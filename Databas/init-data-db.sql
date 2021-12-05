34fc19c042bcbd1c681936e10bf8b39e6c9d4a45
Anna Bergvall <anna.v.bergvall@gmail.com>
2021-12-05 12:06:00 +0100

    initialize hotels and types script

---
+++ Databas/init-data-db.sql
@@ -0,0 +1,70 @@
+-- initialize Types table
+
+INSERT INTO Types (ID, Industry)
+VALUES (1, "Hotell");
+
+INSERT INTO Types (ID, Industry)
+VALUES (2, "Restaurang");
+
+INSERT INTO Types (ID, Industry)
+VALUES (3, "Resebyråer");
+
+
+-- insert data into Users
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("info.marinaplaza@elite.se", "Elite Hotel Marina Plaza", 1, 0);
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("info.mollberg@elite.se", "Elite Hotel Mollberg", 1, 0);
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("cl.grandhelsingborg@choice.se", "Clarion Grand Hotel", 1, 0);
+
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("cl.grandhelsingborg@choice.se", "Clarion Grand Hotel", 1, 0);
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("co.nouveau@choice.se", "Comfort Hotel", 1, 0);
+
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("reception@thevaulthotel.se", "The Vault", 1, 0);
+
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("info@hotellviking.se", "V Hotel", 1, 0);
+
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("info@hotelkarnan.se", "First Hotel Kärnan", 1, 0);
+
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("helsingborg@scandichotels.com", "Scandic Nord", 1, 0);
+
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("helsingborg@gmorninghotels.se", "Good Morning Hotels", 1, 0);
+
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("kathy.adenlof@amasten.se", "Continental Apartments Hotels", 1, 0);
+-- 19 apartments
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("info@hotellstadsparken.se", "Hotel Stadsparken", 1, 0);
+-- 64 rooms
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("linnea@hotell-linnea.se", "Hotell Linnéa", 1, 0);
+-- 37 rooms
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("info@villathalassa.se", "Villa Thalassa", 1, 0);
+-- 34 rooms
+
+INSERT INTO Users (Email, Name, Type_ID, Submitted)
+VALUES ("info@cityvandrarhemmet.com", "Cityvandrarhem", 1, 0);
+-- 41 rooms
+
